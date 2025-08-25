e74fa0c2e9e8b8609809c4f49a8d0adbd2dc641f
osks72916-ops <osks72916@gmail.com>
2025-08-25 10:46:49 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// GitHub Pagesにデプロイするリポジトリ名に変更
-export default defineConfig({
-  plugins: [react()],
-  base: '/my-vocal-app/' // ←GitHubリポジトリ名
-})
