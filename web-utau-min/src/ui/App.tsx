74bbc7b7aa0a9ffa9dcd1116518dbcecefaf6018
osks72916-ops <osks72916@gmail.com>
2025-08-25 10:58:28 +0900

    Delete web-utau-min directory

--- web-utau-min/src/ui/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-export default function App(){ return (<div style={{padding:16}}>
-  <h1>Web UTAU Minimal Starter</h1>
-  <p>ここから実装を進めましょう（oto.ini パーサ、レンダラなど）。</p>
-</div>) }
